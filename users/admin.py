5f036dad63c5a4afdabed0bb9c046ec28b43766a
sairam <ankatasairam2909@gmail.com>
2021-10-01 16:39:55 +0530

    minor changes

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
 from users.models import User, UserResume
 
 
-# class UserAccountAdmin(UserAdmin):
+class UserAccountAdmin(admin.ModelAdmin):
+    list_display = ("username", "email")
 
-admin.site.register(User)
-admin.site.register(UserResume)
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.filter(is_deleted=False)
+        return qs
+
+
+class UserResumeAdmin(admin.ModelAdmin):
+    list_display = ("user_name", "url", "date_of_adding")
+
+    def user_name(self, obj):
+        return obj.user.username
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.filter(user__is_deleted=False)
+        return qs
+
+
+admin.site.register(User, UserAccountAdmin)
+admin.site.register(UserResume, UserResumeAdmin)
 
