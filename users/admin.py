f8507c0c7c99eff2e8a3a1c30f5ac815c7360056
sairam <ankatasairam2909@gmail.com>
2021-10-01 14:43:59 +0530

    adding models to admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User, UserResume
 
-# Register your models here.
+
+# class UserAccountAdmin(UserAdmin):
+
+admin.site.register(User)
+admin.site.register(UserResume)
+
