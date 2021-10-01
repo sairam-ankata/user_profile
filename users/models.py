087e492c24fc57e5b640e5c5dee3e1f05d40c2ef
sairam <ankatasairam2909@gmail.com>
2021-10-01 14:40:34 +0530

    adding models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    is_deleted = models.BooleanField(default=False)
+
+    def __str__(self):
+        return "<User: {}-{}>".format(self.username, self.first_name)
+
+
+class UserResume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    url = models.TextField()
+    date_of_adding = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "<UserResume: {}".format(self.user.username)
